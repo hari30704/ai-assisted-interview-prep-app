25507a27ed1909e085d7d9a135005f1ba1e43ac8
Hari Kumar <harikumar30704@gmail.com>
2025-08-03 01:25:38 +0530

    added the gemini ai and prompts

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,14 +9,14 @@ router.post("/register", registerUser); //Register a new user
 router.post("/login", loginUser);   // Login user
 router.get("/profile", protect, getUserProfile); // Get user profile, protected route
 
-router. post("/upload-image", upload. single("image"), (req, res) => {
-    if (!req. file) {
-      return res. status(400) .json({ message: "No file uploaded" });
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
     }
     const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-        req. file. filename
+        req.file.filename
     }`
-    res.status(200).json({ imageUrl});
-}) ;
+    res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes")
+const {protect} = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation} = require("./controllers/aiController");
+
 const app = express();
 
 // Middleware to handle CORS
@@ -25,10 +29,10 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use('/api/sessions', sessionRoutes);
-// app.use('/api/questions', questionRoutes);
+app.use('/api/questions', questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
